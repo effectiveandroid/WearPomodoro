1c89892975862b7d2d7df6264da6846a92c30e6c
Alex Korovyansky <alexkorovyansky@gmail.com>
2015-01-11 20:32:38 +0600

    Split PomodoroConstants between debug and release

---
+++ wear/src/debug/java/com/alexkorovyansky/wearpomodoro/app/PomodoroConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2014 Alex Korovyansky.
+ */
+package com.alexkorovyansky.wearpomodoro.app;
+
+public class PomodoroConstants {
+    public static final int POMODORO_LENGTH_MS = 1 * 60 * 1000;
+    public static final int SHORT_BREAK_LENGTH_MS = 1 * 30 * 1000;
+    public static final int LONG_BREAK_LENGTH_MS = 1 * 60 * 1000;
+    public static final int POMODORO_NUMBER_FOR_LONG_BREAK = 4;
+}
